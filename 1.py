090ff243efeaf468ec6b4d599a0d2a43c89aaab1
kefirochek <honortv11@gmail.com>
2025-03-10 16:44:22 +0300

    проба пера

---
+++ 1.py
@@ -0,0 +1,32 @@
+import os
+import sys
+
+import pygame
+import requests
+
+server_address = 'https://static-maps.yandex.ru/v1?'
+api_key = 'f3a0fe3a-b07e-4840-a1da-06f18b2ddf13'
+# ll_spn = 'll=149.125531,-35.306907&spn=10,10'
+ll_spn = 'll=50.6424,55.3631&spn=0.091,0.091'
+
+map_request = f"{server_address}{ll_spn}&apikey={api_key}"
+response = requests.get(map_request)
+
+if not response:
+    print("Ошибка выполнения запроса:")
+    print(map_request)
+    print("Http статус:", response.status_code, "(", response.reason, ")")
+    sys.exit(1)
+
+map_file = "map.png"
+with open(map_file, "wb") as file:
+    file.write(response.content)
+
+pygame.init()
+screen = pygame.display.set_mode((600, 450))
+screen.blit(pygame.image.load(map_file), (0, 0))
+pygame.display.flip()
+while pygame.event.wait().type != pygame.QUIT:
+    pass
+pygame.quit()
+os.remove(map_file)
